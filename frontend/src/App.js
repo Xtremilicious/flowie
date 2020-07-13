b266b44ad4eb534bf3bd98c38fe50fd79582a21d
unknown <nilarjundas@outlook.com>
2020-07-13 19:19:05 +0530

    set up dynamic route for user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,13 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
 import Home from "./components/Home/index";
-import Content from "./components/Home/Content";
+import Dashboard from "./components/Dashboard/index";
 
 function App() {
   return (
     <Router>
       <Route exact path="/" component={Home} />
-      <Route exact path="/test" component={Content} />
+      <Route exact path="/user/:userId" component={Dashboard} />
     </Router>
   );
 }
--- frontend/src/components/Dashboard/index.jsx
+++ frontend/src/components/Dashboard/index.jsx
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-export default function index() {
-  return <div></div>;
+export default function Dashboard({ match, location }) {
+  const [userData, setuserData] = useState(null);
+
+  axios.get(`https://api.github.com/users/${match.params.userId}`).then(function (response) {
+    setuserData(response);
+  });
+
+  console.log(match, location);
+  return (
+    <>
+      <code>{JSON.stringify(userData, null, 2)}</code>
+    </>
+  );
 }
