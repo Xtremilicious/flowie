a897541b5f830c68247b5035f96b8618b822148d
unknown <nilarjundas@outlook.com>
2020-07-13 19:08:06 +0530

    add axios dependency

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
 import Home from "./components/Home/index";
+import Content from "./components/Home/Content";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/test" component={Content} />
+    </Router>
+  );
 }
 
 export default App;
