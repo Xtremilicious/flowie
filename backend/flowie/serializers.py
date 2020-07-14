d0094fba73e97ba607aef3368115fb229b60ddb0
Diaga <diagaaos@gmail.com>
2020-07-14 19:13:32 +0500

    Add Tracked Repository Serializer

--- backend/flowie/serializers.py
+++ backend/flowie/serializers.py
@@ -5,3 +5,8 @@ from core.models import TrackedRepository
 
 class TrackedRepositorySerializer(serializers.ModelSerializer):
     """Serializer for Tracked Repository model"""
+
+    class Meta:
+        model = TrackedRepository
+        fields = ('id', 'user', 'repo_name')
+        read_only_fields = ('id', )
