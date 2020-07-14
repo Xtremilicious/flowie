a04ffad5ad4fd421b3dc9fb7304e6984401f4cc4
Diaga <diagaaos@gmail.com>
2020-07-14 15:23:46 +0500

    Add user model

---
+++ backend/core/tests/test_models.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from core import models_utils as mu
+
+
+class UserModelTests(TestCase):
+    """Tests for user model"""
+
+    def test_create_user(self):
+        """Test creating a user"""
+        user = mu.sample_user()
+
+        self.assertEqual(user.name, "sample_user")
+        self.assertTrue(user.check_password("sample_user"))
+
+    def test_user_name_lower(self):
+        """Test user name is lower cased"""
+        name = "SAMPLE"
+        user = mu.sample_user(name=name)
+
+        self.assertEqual(user.name, name.lower())
